c8d52de8aaee9f36b18afba001aa4414fa5879a3
Tracy Wei <66813711+tracyywei@users.noreply.github.com>
2021-07-07 17:04:19 -0500

    improved k-means and mean-shift

--- breastcancer-clustering.py
+++ breastcancer-clustering.py
@@ -9,84 +9,92 @@ Created on Mon Jul  5 17:36:55 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
 from sklearn.datasets import load_breast_cancer
 dataset = load_breast_cancer()
-df = dataset.data
-df_target = pd.DataFrame(dataset.target, columns=['target'])
-df_features = pd.DataFrame(dataset.data, columns = dataset.feature_names)
 X = dataset.data
-y = dataset.target
+Y = dataset.target
 
-from sklearn import preprocessing
-normalizedX = preprocessing.normalize(X)
+#pX = preprocessing.normalize(X)
+pX = StandardScaler().fit_transform(X)
 
 # k-means clustering
 from sklearn.cluster import KMeans
-model = KMeans(n_clusters = 2)
-model.fit_predict(normalizedX)
-k_labels = model.labels_
+model = KMeans(n_clusters=2, init='k-means++', n_init=10, max_iter=300)
+k_pred = model.fit_predict(pX)
 
-plt.scatter(X[:,0], X[:,1], c = k_labels, alpha = 0.5)
+df = pd.DataFrame({'prediction': k_pred, 'ground-truth': Y})
+ct = pd.crosstab(df['prediction'], df['ground-truth'])
+print(ct) 
+
+y_pred = np.zeros((569,))
+y_pred[np.where(k_pred==0)]= 1
+y_pred[np.where(k_pred==1)]= 0
+
+print('K-means Clustering accuracy score:')
+print(accuracy_score(Y, y_pred))
+cm = confusion_matrix(Y, y_pred)
+print(cm)
+
+plt.scatter(pX[:,0], pX[:,1], c = y_pred, alpha = 0.5)
 plt.title('k-means clustering')
 plt.xlabel(dataset.feature_names[0])
 plt.ylabel(dataset.feature_names[1])
 plt.show()
-
-cm = confusion_matrix(dataset.target, predictions)
-print('K-means Clustering accuracy score:')
-print(accuracy_score(dataset.target, k_labels))
-print(cm)
 
 
 # Hierarchical clustering
 
 from sklearn.cluster import AgglomerativeClustering 
 hc = AgglomerativeClustering(n_clusters = 2, affinity = 'euclidean', linkage = 'ward')
-hc.fit_predict(normalizedX)
-labels = hc.labels_
+h_pred = hc.fit_predict(pX)
 
-dframe = pd.DataFrame({'predictions': labels, 'target': dataset.target})
+dframe = pd.DataFrame({'predictions': h_pred, 'target': Y})
 ct = pd.crosstab(dframe['predictions'], dframe['target'])
 print(ct)
 
-plt.scatter(X[:,0], X[:,1], c = labels, alpha = 0.5)
+y_pred = np.zeros((569,))
+y_pred[np.where(h_pred==0)]= 0
+y_pred[np.where(h_pred==1)]= 1
+
+print('Hierarchical clustering accuracy score:')
+print(accuracy_score(Y, h_pred))
+cm = confusion_matrix(Y, h_pred)
+print(cm)
+
+plt.scatter(X[:,0], X[:,1], c = h_pred, alpha = 0.5)
 plt.title('Hierarchical clustering')
 plt.xlabel(dataset.feature_names[0])
 plt.ylabel(dataset.feature_names[1])
 plt.show()
 
-cm = confusion_matrix(dataset.target, labels)
-print('Hierarchical clustering accuracy score:')
-print(accuracy_score(dataset.target, labels))
-print(cm)
 
 # Mean-shift
-from sklearn.cluster import MeanShift, estimate_bandwidth
-bandwidth = estimate_bandwidth(df, quantile=0.7, n_samples=569)
-ms = MeanShift(bandwidth=bandwidth)
-ms.fit(df)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
-ms_pred = ms.predict(df)
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
+from sklearn.cluster import MeanShift
+ms = MeanShift()
+m_pred = ms.fit_predict(pX)
 
-from itertools import cycle
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(df[my_members, 0], df[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-plt.title('Mean-shift clustering')
-plt.show()
-cm = confusion_matrix(dataset.target, ms_pred)
+dframe = pd.DataFrame({'predictions': m_pred, 'target': Y})
+ct = pd.crosstab(dframe['predictions'], dframe['target'])
+print(ct)
+
+my_pred = np.zeros((569,))
+my_pred[np.where(m_pred==0)]= 1
+my_pred[np.where(m_pred==11)]= 1
+my_pred[np.where(m_pred==12)]= 1
+my_pred[np.where(m_pred==13)]= 1
+
 print('Mean-shift accuracy score:')
-print(accuracy_score(dataset.target, ms_pred))
+print(accuracy_score(Y, my_pred))
+cm = confusion_matrix(Y, my_pred)
 print(cm)
+
+plt.scatter(X[:,0], X[:,1], c = my_pred, alpha = 0.5)
+plt.title('Mean-shift clustering')
+plt.xlabel(dataset.feature_names[0])
+plt.ylabel(dataset.feature_names[1])
+plt.show()
