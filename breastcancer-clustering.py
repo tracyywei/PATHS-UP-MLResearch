1a3c9dc74d6200cf93102fcd4e649c649b88db3e
Tracy Wei <66813711+tracyywei@users.noreply.github.com>
2021-07-07 00:22:03 -0500

    improved hierarchical clustering accuracy

--- breastcancer-clustering.py
+++ breastcancer-clustering.py
@@ -10,7 +10,6 @@ Created on Mon Jul  5 17:36:55 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
@@ -23,7 +22,7 @@ df_target = pd.DataFrame(dataset.target, columns=['target'])
 df_features = pd.DataFrame(dataset.data, columns = dataset.feature_names)
 
 # k-means clustering
-from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import KMeans
 model = KMeans(n_clusters = 2)
 model.fit(df)
 predictions = model.predict(df)
@@ -55,28 +54,31 @@ print(cm)
 
 
 # Hierarchical clustering
-from sklearn.cluster import AgglomerativeClustering 
-hc = AgglomerativeClustering(n_clusters=2)
-hc_pred = hc.fit_predict(df)
+X = dataset.data
+y = dataset.target
 
-dframe = pd.DataFrame({'predictions': hc_pred, 'target': dataset.target})
+from sklearn import preprocessing
+normalizedX = preprocessing.normalize(X)
+
+from sklearn.cluster import AgglomerativeClustering 
+hc = AgglomerativeClustering(n_clusters = 2, affinity = 'euclidean', linkage = 'ward')
+hc.fit_predict(normalizedX)
+labels = hc.labels_
+
+dframe = pd.DataFrame({'predictions': labels, 'target': dataset.target})
 ct = pd.crosstab(dframe['predictions'], dframe['target'])
 print(ct)
 
-hc_pred_labels = np.zeros((569,))
-hc_pred_labels[np.where(predictions ==0)] = 1
-hc_pred_labels[np.where(predictions ==1)] = 0
-
-plt.scatter(df[:,0], df[:,1], c = hc_pred_labels, alpha = 0.5)
+plt.scatter(X[:,0], X[:,1], c = labels, alpha = 0.5)
 plt.title('Hierarchical clustering')
 plt.xlabel(dataset.feature_names[0])
 plt.ylabel(dataset.feature_names[1])
 plt.show()
-cm = confusion_matrix(dataset.target, hc_pred_labels)
-print('Hierarchical clustering accuracy score:')
-print(accuracy_score(dataset.target, hc_pred_labels))
-print(cm)
 
+cm = confusion_matrix(dataset.target, labels)
+print('Hierarchical clustering accuracy score:')
+print(accuracy_score(dataset.target, labels))
+print(cm)
 
 # Mean-shift
 from sklearn.cluster import MeanShift, estimate_bandwidth
